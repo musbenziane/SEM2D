a1f63b5662820d73006671a73d3fd5fda249ca71
musbenziane <medbenziane1992@gmail.com>
2021-11-05 19:26:57 +0100

    moved folders

---
+++ build/CMakeFiles/SEM2D.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/mus/Documents/Modelling/MyProjects/SEM2D/buildmesh.f90" "/home/mus/Documents/Modelling/MyProjects/SEM2D/build/CMakeFiles/SEM2D.dir/buildmesh.f90.o"
+  "/home/mus/Documents/Modelling/MyProjects/SEM2D/connectivity_matrix.f90" "/home/mus/Documents/Modelling/MyProjects/SEM2D/build/CMakeFiles/SEM2D.dir/connectivity_matrix.f90.o"
+  "/home/mus/Documents/Modelling/MyProjects/SEM2D/gll.f90" "/home/mus/Documents/Modelling/MyProjects/SEM2D/build/CMakeFiles/SEM2D.dir/gll.f90.o"
+  "/home/mus/Documents/Modelling/MyProjects/SEM2D/main.f90" "/home/mus/Documents/Modelling/MyProjects/SEM2D/build/CMakeFiles/SEM2D.dir/main.f90.o"
+  "/home/mus/Documents/Modelling/MyProjects/SEM2D/shapefunc2D.f90" "/home/mus/Documents/Modelling/MyProjects/SEM2D/build/CMakeFiles/SEM2D.dir/shapefunc2D.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SEM2D.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SEM2D.dir/buildmesh.f90.o"
+  "CMakeFiles/SEM2D.dir/connectivity_matrix.f90.o"
+  "CMakeFiles/SEM2D.dir/gll.f90.o"
+  "CMakeFiles/SEM2D.dir/main.f90.o"
+  "CMakeFiles/SEM2D.dir/shapefunc2D.f90.o"
+  "SEM2D"
+  "SEM2D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/SEM2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
